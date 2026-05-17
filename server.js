bab341f71cb11e35f14e5435a8d086b72940352e
chintu <mudilichaitanya@gmail.com>
2026-05-17 11:43:17 +0530

    chore: update database credentials and restrict CORS origin to production frontend URL

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const { sequelize } = require('./models');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors()); // Allow all origins in dev
+
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://stockflow.vercel.app', // your Vercel URL
+    origin: 'https://stockflow-frontend-seven.vercel.app',
     credentials: true
 }));
 // Routes
